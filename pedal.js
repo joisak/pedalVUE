77990afaf1b1c2c9bb29b288667f4f442320d3e5
Joakim <joakim.isaksson@lodon.se>
2017-06-30 15:22:25 +0200

    UPDATE

--- pedal.js
+++ pedal.js
@@ -3,17 +3,27 @@ var user = "joisak",
 new Vue({
 	el: '#music-list',
 	data: {
-		results: [],
-		topTracks : [],
+		resultsTopTracks: [],
+		resultsTopArtist: [],
+		topTracks: [],
+		topArtist:[],
 		loading: false,
 		show: false,
+		collapse: false
 	},
 	created() {
 		axios.get('http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user='+user+'&api_key='+ apiKey +'&format=json')
 		.then(response => {
-			this.results = response.data.recenttracks.track
+			this.resultsTopTracks = response.data.recenttracks.track
 			for(var i = 0; i < 9; i++){
-				this.topTracks.push(this.results[i]);
+				this.topTracks.push(this.resultsTopTracks[i]);
+			}
+		});
+		axios.get('http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user='+user+'&api_key='+ apiKey +'&format=json')
+		.then(response => {
+			this.resultsTopArtist = response.data.topartists.artist
+			for(var i = 0; i < 9; i++){
+				this.topArtist.push(this.resultsTopArtist[i]);
 			}
 		});
 
