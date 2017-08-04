7e3b24ab889dda4468b6a13dd0c27c4cb04ef384
Joakim <joakim.isaksson@lodon.se>
2017-08-04 14:12:51 +0200

    adding new files to repo

--- pedal.js
+++ pedal.js
@@ -4,12 +4,9 @@ new Vue({
 	el: '#music-list',
 	data: {
 		results: [],
-		resultsArtist: [],
-		topArtist: [],
 		topTracks : [],
 		loading: false,
 		show: false,
-		showArtist: false
 	},
 	created() {
 		axios.get('http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user='+user+'&api_key='+ apiKey +'&format=json')
@@ -19,24 +16,9 @@ new Vue({
 				this.topTracks.push(this.results[i]);
 			}
 		});
-		axios.get('http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user='+user+'&api_key='+ apiKey +'&format=json')
-		.then(response => {
-			this.resultsArtist = response.data.topartists.artist
-			for(var i = 0; i < 9; i++){
-				this.topArtist.push(this.resultsArtist[i]);
-			}
-		});
-	},
-	methods: {
-		buttonArtist: function(){
-					this.show = false;
-					this.showArtist = true;
-		},
-		buttonTracks: function(){
-					this.showArtist = false;
-					this.show = true;
-		}
+
 	},
 	updated(){
+			this.show = true;
 	}
 })
