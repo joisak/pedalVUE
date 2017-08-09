0ed3ff7c8e865becf5f42a61157762965e957673
Joakim <joakim.isaksson@lodon.se>
2017-08-09 11:45:39 +0200

    UPDATE index.html, pedal.js

--- pedal.js
+++ pedal.js
@@ -1,12 +1,11 @@
 var user = "joisak",
-		apiKey = "2716701230e056321ef598407d964f0b";
+  apiKey = "2716701230e056321ef598407d964f0b";
 
 const Tracks = {
-	template: `
+  template: `
 	<div>
 		<div class="text-center" v-for="item in topTracks">
 			<p v-show="loading">"Listen to a lot of music..."</p>
-
 						<transition name="slide">
 							<div class="music-shit" v-if="show">
 								<img :src="item.image[3]['#text']"/>
@@ -16,58 +15,95 @@ const Tracks = {
 								</p>
 							</div>
 						</transition>
-
 		</div>
 	</div>
 	`,
-	data(){
-		return {
-			results: [],
-			topTracks : [],
-			loading: false,
-			show: false,
-			seen: false
-		}
-	},
-	created() {
-		axios.get('http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user='+user+'&api_key='+ apiKey +'&format=json')
-		.then(response => {
-			this.results = response.data.recenttracks.track
-			for(var i = 0; i < 9; i++){
-				this.topTracks.push(this.results[i]);
-			}
-		});
-	},
-	updated(){
-		console.log(this.$children);
-		this.show = true;
-	}
+  data() {
+    return {
+      results: [],
+      topTracks: [],
+      loading: false,
+      show: false,
+      seen: false
+    }
+  },
+  created() {
+    axios.get('http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=' + user + '&api_key=' + apiKey + '&format=json')
+      .then(response => {
+        this.results = response.data.recenttracks.track
+        for (var i = 0; i < 9; i++) {
+          this.topTracks.push(this.results[i]);
+        }
+      });
+  },
+  updated() {
+    this.show = true;
+  }
 };
 
-const Home = {
-	template: `
+const Me = {
+  template: `
 		<transition name="fade">
-	<div class="text-center">
-
-					<img src="me.jpg" class="me-img">
-					<span>Web enthusiast with a passion to develop and design web pages. My expertise consists of the combination of editorial work, design and programming.  I am a cheerful, sociable and cool person with an interest in communication.
+			<div class="text-center">
+				<img src="me.jpg" class="me-img">
+				<span>
+					Web enthusiast with a passion to develop and design web pages. My expertise consists of the combination of editorial work, design and programming.  I am a cheerful, sociable and cool person with an interest in communication.
 					If you would like more information or to discuss commissions and collaborations, please get in touch.
 					Joakim Isaksson Göteborg
-					</span>
-	</div>
-	</transition>
+				</span>
+			</div>
+		</transition>
+	`,
+  data() {
+    return {
+      show: false,
+    }
+  }
+}
 
+
+
+Vue.component('links', {
+	template: `
+		<div>
+			<transition name="fade">
+			<div v-if="show">
+						<div v-if="tracks" v-on:click="changeLink">
+							<router-link to="/me">ME</router-link>
+						</div>
+						<div v-if="!tracks" v-on:click="changeLink">
+								<router-link v-on:click="changeLink" to="/">Tracks</router-link>
+						</div>
+					</transition-group>
+			</div>
+			</transition>
+		</div>
 	`,
 	data(){
 		return {
-			show: false
+			show: false,
+			tracks: true
 		}
+	},
+	methods: {
+		changeLink: function(){
+			this.tracks = (this.tracks) ? this.tracks = false : this.tracks = true;
+		}
+	},
+	mounted() {
+		this.show = true;
 	}
-}
 
-const routes = [
-  { path: '/foo', component: Tracks },
-  { path: '/bar', component: Home }
+});
+
+const routes = [{
+    path: '/',
+    component: Tracks
+  },
+  {
+    path: '/me',
+    component: Me
+  }
 ]
 
 const router = new VueRouter({
@@ -76,6 +112,6 @@ const router = new VueRouter({
 
 
 new Vue({
-	el: '#music-list',
-	router
+  el: '#music-list',
+  router
 })
